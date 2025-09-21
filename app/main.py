9c3265a38e8c2ecde1e87ddd8e50cb6df1463d91
Olena Pyslar <pislar33@gmail.com>
2025-09-21 22:20:53 +0300

    Solution 3

--- app/main.py
+++ app/main.py
@@ -8,10 +8,7 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    people_list = []
-    for person in people:
-        person = Person(person["name"], person["age"])
-        people_list.append(person)
+    people_list = [Person(person["name"], person["age"]) for person in people]
     for person in people:
         person_instance = Person.people[person["name"]]
         if person.get("wife"):
